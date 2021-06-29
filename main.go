54724f9735eaef7ccaa00c76dfbf69862d112c4a
Rougeon <corentin.rougeon@ynov.com>
2021-06-29 15:44:59 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+/*type Page struct{
+	Title	string
+	Articles []string
+}*/
+
+func serverweb(w http.ResponseWriter, r *http.Request) {
+	file := "index.html"
+	tmpl := template.New("index.html")
+	tmpl = template.Must(tmpl.ParseFiles("index.html"))
+	tmpl.ExecuteTemplate(w, "index.html", file)
+}
+
+func main() {
+	server := http.NewServeMux()
+	server.HandleFunc("/", serverweb)
+	http.ListenAndServe(":8080", logerror(server))
+}
+
+func logerror(getlogerror http.Handler) http.Handler {
+	a := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.RemoteAddr, "\t", r.RequestURI, r.Method)
+		getlogerror.ServeHTTP(w, r)
+
+		fmt.Println(r.FormValue("num1"),r.FormValue("num2"),r.FormValue("operator"))
+
+		num1, _ := strconv.Atoi(r.FormValue("num1"))
+		num2, _ := strconv.Atoi(r.FormValue("num2"))
+		Res := 0
+
+		switch r.FormValue("operator") {
+		case "add":Res =num1+num2
+		case "sub":Res =num1-num2
+		case "mult":Res =num1*num2
+		case "div":Res =num1/num2
+		}
+
+		fmt.Println(Res)
+		//fmt.Println(num1+num2)
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		_, _ = fmt.Fprint(w,"your result : <br>"  + strconv.Itoa(Res))
+	}
+	return http.HandlerFunc(a)
+
+}
+
