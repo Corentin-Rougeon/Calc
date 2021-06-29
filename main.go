99c0ec9d60717825a9089b17770164257a9b22fb
Rougeon <corentin.rougeon@ynov.com>
2021-06-29 17:16:15 +0200

    first commit

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-func serverweb(w http.ResponseWriter, r *http.Request) {
+func serverweb(w http.ResponseWriter, _ *http.Request) {
 	file := "index.html"
 	tmpl := template.New("index.html")
 	tmpl = template.Must(tmpl.ParseFiles("index.html"))
@@ -17,19 +17,21 @@ func serverweb(w http.ResponseWriter, r *http.Request) {
 func main() {
 	server := http.NewServeMux()
 	server.HandleFunc("/", serverweb)
-	http.ListenAndServe(":8080", logerror(server))
+	http.ListenAndServe(":8080", log(server))
 }
 
-func logerror(getlogerror http.Handler) http.Handler {
+func log(getlogerror http.Handler) http.Handler {
 	a := func(w http.ResponseWriter, r *http.Request) {
+
 		fmt.Println(r.RemoteAddr, "\t", r.RequestURI, r.Method)
 		getlogerror.ServeHTTP(w, r)
-
 		fmt.Println(r.FormValue("num1"), r.FormValue("num2"), r.FormValue("operator"))
 
-		num1, _ := strconv.Atoi(r.FormValue("num1"))
-		num2, _ := strconv.Atoi(r.FormValue("num2"))
-		Res := 0
+
+		num1, _ := strconv.ParseFloat(r.FormValue("num1"),64)
+		num2, _ := strconv.ParseFloat(r.FormValue("num2"),64)
+
+		var Res float64
 
 		switch r.FormValue("operator") {
 		case "add":
@@ -45,8 +47,12 @@ func logerror(getlogerror http.Handler) http.Handler {
 		fmt.Println(Res)
 
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		_, _ = fmt.Fprint(w, "your result : <br>"+strconv.Itoa(Res))
+
+		_, _ = fmt.Fprint(w, "your result : <br>"+fmt.Sprintf("%f", Res))
+
 	}
 	return http.HandlerFunc(a)
 
 }
+
+
